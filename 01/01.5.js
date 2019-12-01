ecc7d99ea23953c7bf1ef1d0b074f1397f83c259
Chris Grimmett <chris@grimtech.net>
2019-11-30 23:01:55 -0800

    add 01

--- 01/01.5.js
+++ 01/01.5.js
@@ -5,14 +5,28 @@ fsp = fs.promises;
 
 let fuelAlgo = (mass) => {
   let f = (Math.floor(mass / 3) - 2);
+  if (f < 1) return 0;
   return f;
 };
 
+//1969 => (654 => 216 => 70 => 21 => 5) => 966
 
-console.log(fuelAlgo(12));
-console.log(fuelAlgo(14));
-console.log(fuelAlgo(1969));
-console.log(fuelAlgo(100756));
+//var fuel = fuelAlgo(1969);
+let moduleFuels = [];
+let calcExtraFuels = (acc, mass) => {
+  if (mass > 0) {
+    let extraFuel = (fuelAlgo(mass));
+    return calcExtraFuels((acc+extraFuel), extraFuel);
+  }
+  else return acc;
+}
+
+
+
+
+console.log(calcExtraFuels(0, 14));
+console.log(calcExtraFuels(0, 1969));
+console.log(calcExtraFuels(0, 100756));
 
 fsp.readFile('./01-input.txt', { encoding: 'utf-8' })
   .then((input) => {
@@ -30,7 +44,7 @@ fsp.readFile('./01-input.txt', { encoding: 'utf-8' })
   })
   .then((modules) => {
     return modules.reduce((acc, moduleMass) => {
-      let moduleFuel = fuelAlgo(moduleMass);
+      let moduleFuel = calcExtraFuels(0, moduleMass);
       return (acc + moduleFuel);
     }, 0);
   })
